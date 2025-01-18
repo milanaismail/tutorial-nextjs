17c6f5bad2bc1b66be1ddd21330b90f40386fd2f
milanaismail <133760937+milanaismail@users.noreply.github.com>
2025-01-18 20:01:04 +0100

    import

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,6 +2,8 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
+import { LatestInvoice } from '@/app/lib/definitions'; // Adjust the path as needed
+
 
 export default async function LatestInvoices({
   latestInvoices,
